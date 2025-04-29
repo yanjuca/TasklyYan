d486ceea467c922119732054dfa5c097facf1497
Ezequielsaraujo <ezequielsaraujo0@gmail.com>
2025-04-29 13:28:44 -0300

    feat: add navigation system with React Navigation

---
+++ src/pages/singUp/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+
+export default function SingUp (){
+    return(
+        <View>
+            <Text>
+                tela cadastro
+            </Text>
+        </View>
+    )
+}
