3b90fc39d5205fc8296a9c4a58c766e19dcdc677
Ezequielsaraujo <ezequielsaraujo0@gmail.com>
2025-04-29 14:47:04 -0300

    fix: bug fix

--- src/pages/selectionScreen/index.tsx
+++ src/pages/selectionScreen/index.tsx
@@ -18,7 +18,7 @@ export default function SelectionScreen (){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'center',  
       alignItems: 'center',      
     },
   });
