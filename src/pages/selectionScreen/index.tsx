fc468c184eee5e759412ce699cf5d508d3ef0a70
Ezequielsaraujo <ezequielsaraujo0@gmail.com>
2025-04-29 13:28:44 -0300

    feat: add navigation system with React Navigation

---
+++ src/pages/selectionScreen/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View,Text, Button, ScrollView, StyleSheet,TouchableOpacity } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+
+export default function SelectionScreen (){
+    const navigation = useNavigation();
+
+    return(
+            <View style={styles.container}>                
+                    <Button title="SingIn" onPress={() => navigation.navigate("SingIn")}></Button>
+                    <Button title="SingUp" onPress={() => navigation.navigate("SingUp")}></Button>
+                    <Button title="SingUp" onPress={() => navigation.navigate("SingUp")}></Button>
+            </View>        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',     
+    },
+  });
