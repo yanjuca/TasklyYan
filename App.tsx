c02dd50661a9a2bf3a7d479885c80866ce866464
Beatriz Souza <beatriz.rodrigues.souza127@gmail.com>
2025-05-06 16:03:59 -0300

    fix: improve modal layout and add new functionalities for theme selection

--- App.tsx
+++ App.tsx
@@ -1,28 +1,31 @@
 import React from "react"; 
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SingIn from "./src/pages/singin";
-import SingUp from "./src/pages/singUp";
-import SelectionScreen from "./src/pages/selectionScreen";
-import SplashScreen from "./src/pages/splashscreen";
-import PreferencesMenu from "./src/pages/preferencesMenu";
+import SingIn from './src/pages/singin';
+import SingUp from './src/pages/singUp';
+import SelectionScreen from './src/pages/selectionScreen';
+import SplashScreen from './src/pages/splashscreen';
+import PreferencesMenu from './src/pages/preferencesMenu';
+import { ThemeProvider } from './src/pages/preferencesMenu/themeContext'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function app(){
   return(
-    <NavigationContainer>
-      <Stack.Navigator>
+    <ThemeProvider> 
+      <NavigationContainer>
+        <Stack.Navigator>
 
-        <Stack.Screen name="SelectionScreen" component={SelectionScreen}/>
-        <Stack.Screen name="SingIn" component={SingIn}/>
-        <Stack.Screen name="SingUp" component={SingUp}/>
-        <Stack.Screen name="SplashScreen" component={SplashScreen}/>
-        <Stack.Screen name="PreferencesMenu" component={PreferencesMenu}/>
+          <Stack.Screen name="SelectionScreen" component={SelectionScreen}/>
+          <Stack.Screen name="SingIn" component={SingIn}/>
+          <Stack.Screen name="SingUp" component={SingUp}/>
+          <Stack.Screen name="SplashScreen" component={SplashScreen}/>
+          <Stack.Screen name="PreferencesMenu" component={PreferencesMenu}/>
         
-      </Stack.Navigator>
-    </NavigationContainer>
+        </Stack.Navigator>
+     </NavigationContainer>
+    </ThemeProvider> 
   )
 }
